2528f8d5fe4e1f0e82d86793c96d959c1631b1ac
heavennebho <102846003+heavennebho@users.noreply.github.com>
2022-10-20 11:20:21 +0530

    Add files via upload

---
+++ Nebho/pack/four.java
@@ -0,0 +1,25 @@
+package pack;
+
+public class four {
+	public static void main(String[] args){
+	    
+		   
+	    int a = 7, b = 11;
+
+	    
+	    System.out.println("a is " + a + " and b is " + b);
+
+	    System.out.println(a == b);  
+
+	    
+	    System.out.println(a != b); 
+	 
+	    System.out.println(a > b);  
+	    
+	    System.out.println(a < b); 
+	   
+	    System.out.println(a >= b);
+       System.out.println(a <= b);   
+	}
+
+}
---
+++ Nebho/pack/six.java
@@ -0,0 +1,15 @@
+package pack;
+
+public class six {
+	public static void main(String[] args) {
+
+	    int number = 10;
+
+	    if (number < 0) {
+	      System.out.println("The number is negative.");
+	    }
+
+	    System.out.println("Statement outside if block");
+	  }
+
+}
